b283f268a6397d6cd782718e9362f7e60bae8f2f
ebgarcia <ebgarcia@sudamericano.edu.ec>
2025-02-27 00:22:38 -0500

    update home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { InformationComponent } from './pages/information/information.component';
 import { ThemesComponent } from './pages/themes/themes.component';
-import { SpeakersComponent } from './pages/speakers/speakers.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
@@ -11,7 +10,6 @@ const routes: Routes = [
   { path: 'home', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
   { path: 'information', loadComponent: () => import('./pages/information/information.component').then(m => m.InformationComponent) },
   { path: 'themes', loadComponent: () => import('./pages/themes/themes.component').then(m => m.ThemesComponent) },
-  { path: 'speakers', loadComponent: () => import('./pages/speakers/speakers.component').then(m => m.SpeakersComponent) },
   { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) }
 ];
 
