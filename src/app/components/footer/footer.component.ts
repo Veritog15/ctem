ffde9a1182a4a08c6b93118715a4440d0ba04b7d
ebgarcia <ebgarcia@sudamericano.edu.ec>
2025-03-16 21:07:21 -0500

    update pages

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
+constructor(private router: Router) {}
+
+  navigate1() {
+    this.router.navigate(['home']);
+  }
+
+  navigate2() {
+    this.router.navigate(['information']);
+  }
+
+  navigate3() {
+    this.router.navigate(['register']);
+  }
+
+  navigate4() {
+    this.router.navigate(['information']);
+  }
+
+  navigate5() {
+    this.router.navigate(['information']);
+  }
+
+  navigate6() {
+    this.router.navigate(['information']);
+  }
+
+  navigate7() {
+    this.router.navigate(['information']);
+  }
 
 }
